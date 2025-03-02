1e0e4601fd2a89044a824406fc780aae4538db54
DouShaoxun <dousx_cn@163.com>
2025-03-02 09:17:05 +0800

    feat(dynamic-config): 升级 Redisson 依赖并增强配置接口- 将 Redisson Spring Boot Starter 依赖从 3.44.0 升级到 3.45.0 - 在 DynamicConfig 注解中添加 Component 注解，使其成为 Spring 组件- 更新项目版本号至 1.1.20250302-09

--- dynamic-config-redis-autoconfigure/src/main/java/cn/cruder/dousx/dcredis/annotation/DynamicConfig.java
+++ dynamic-config-redis-autoconfigure/src/main/java/cn/cruder/dousx/dcredis/annotation/DynamicConfig.java
@@ -1,5 +1,7 @@
 package cn.cruder.dousx.dcredis.annotation;
 
+import org.springframework.stereotype.Component;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,6 +10,7 @@ import java.lang.annotation.Target;
 /**
  * 被该注解标记的接口为配置接口
  */
+@Component
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DynamicConfig {
